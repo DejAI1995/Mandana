faf19e7981e30846a5f23d989f8b8c657f0794f7
DejAI1995 <ayodejiodunela@hotmail.com>
2025-05-22 21:59:57 +0100

    Initial Commit

---
+++ Mandana/MandanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandanaApp.swift
+//  Mandana
+//
+//  Created by Deji O on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MandanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MandanaTests/MandanaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MandanaTests.swift
+//  MandanaTests
+//
+//  Created by Deji O on 22/05/2025.
+//
+
+import Testing
+@testable import Mandana
+
+struct MandanaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
